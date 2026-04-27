c8b9d03ed96614035f1fa76cd301c6078806b81a
JackArnold6 <jarnold6@cord.edu>
2026-04-27 16:24:54 -0500

    made the outline for courses page but nee to merge calculator and attendance tracker once done

---
+++ src/main/java/com/betterstudentteam/thebetterstudentapp/Courses/Course.java
@@ -0,0 +1,64 @@
+
+
+package com.betterstudentteam.thebetterstudentapp.Courses;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+
+
+public class Course {
+
+    private String id;
+    private String name;
+    private String instructor;
+    private List<DayOfWeek> meetingDays;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    private double currentGrade;  // this is calculated from Assignments
+
+
+    public Course(String id, String name, String instructor, List<DayOfWeek> meetingDays, LocalTime startTime, LocalTime endTime) {
+
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+        this.meetingDays = meetingDays;
+        this.startTime = startTime;
+        this.endTime = endTime;
+
+        this.currentGrade = 0.0; // placeholder until Grade Calculator is merged
+
+    }
+
+//    Getters
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getInstructor() {
+        return instructor;
+    }
+    public List<DayOfWeek> getMeetingDays() {
+        return meetingDays;
+    }
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+    public double getCurrentGrade() {
+        return currentGrade;
+    }
+
+//    Setters
+    public void setCurrentGrade(double currentGrade) {
+        this.currentGrade = currentGrade;
+    }
+
+    
+}
