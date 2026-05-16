66c4610d318be8f18e2f63d9df806b5c21f157b6
miguelgarc5605 <miguelgarc5605@gmail.com>
2026-05-15 20:04:47 -0500

    added setup add-course button w/ css styling

--- src/main/java/com/betterstudentteam/thebetterstudentapp/frontend/SetupPane.java
+++ src/main/java/com/betterstudentteam/thebetterstudentapp/frontend/SetupPane.java
@@ -1,11 +1,13 @@
 package com.betterstudentteam.thebetterstudentapp.frontend;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class SetupPane extends VBox {
 
     private Label mHeader;
+    private Button mAddCourseButton;
 
     public SetupPane() {
         this.getStyleClass().add("pane_setup");
@@ -13,5 +15,10 @@ public class SetupPane extends VBox {
         mHeader = new Label("Setup Course");
         mHeader.getStyleClass().add("label_setup_header");
         this.getChildren().add(mHeader);
+
+        mAddCourseButton = new Button("Add Course");
+        mAddCourseButton.getStyleClass().add("button_setup_add-course");
+        this.getChildren().add(mAddCourseButton);
+
     }
 }
