d62094ceef9288de8f5d7a3f84d91f8abfb884b3
miguelgarc5605 <miguelgarc5605@gmail.com>
2026-05-21 11:04:43 -0500

    added TaskVBox for reusability

---
+++ src/main/java/com/betterstudentteam/thebetterstudentapp/frontend/todo/TaskVBox.java
@@ -0,0 +1,25 @@
+package com.betterstudentteam.thebetterstudentapp.frontend.todo;
+
+import atlantafx.base.theme.Styles;
+import com.betterstudentteam.thebetterstudentapp.backend.Display;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.VBox;
+
+public class TaskVBox extends VBox {
+
+    private Label mHeader;
+    private Separator mSeperator;
+
+    public TaskVBox(String name) {
+        mHeader = new Label(name);
+        mHeader.getStyleClass().add(Styles.TITLE_1);
+        this.getChildren().add(mHeader);
+
+        mSeperator = new Separator(Orientation.HORIZONTAL);
+        this.getChildren().add(mSeperator);
+
+        this.setMinWidth(Display.SCREEN_BOUNDS.getWidth() / 3.0);
+    }
+}
--- src/main/java/com/betterstudentteam/thebetterstudentapp/frontend/todo/TodoPane.java
+++ src/main/java/com/betterstudentteam/thebetterstudentapp/frontend/todo/TodoPane.java
@@ -1,47 +1,24 @@
 package com.betterstudentteam.thebetterstudentapp.frontend.todo;
 
-import atlantafx.base.theme.Styles;
-import com.betterstudentteam.thebetterstudentapp.backend.Display;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 
 public class TodoPane extends BorderPane {
 
-    private VBox mAllTodoPanel;
-    private VBox mBacklogTodoPanel;
-    private VBox mDailyTodoPanel;
-
-    private Label mAllTodoHeader;
-    private Label mBacklogTodoHeader;
-    private Label mDailyTodoHeader;
+    private TaskVBox mAllTodoPanel;
+    private TaskVBox mBacklogTodoPanel;
+    private TaskVBox mDailyTodoPanel;
 
     public TodoPane() {
-        mAllTodoHeader = new Label("All Todo");
-        mAllTodoHeader.getStyleClass().add(Styles.TITLE_1);
-
-        mBacklogTodoHeader = new Label("Backlog Todo");
-        mBacklogTodoHeader.getStyleClass().add(Styles.TITLE_1);
-
-        mDailyTodoHeader = new Label("Daily Todo");
-        mDailyTodoHeader.getStyleClass().add(Styles.TITLE_1);
-
-        mAllTodoPanel = new VBox();
+        mAllTodoPanel = new TaskVBox("All Todo");
         mAllTodoPanel.getStyleClass().add("panel_all-todo");
-        mAllTodoPanel.getChildren().add(mAllTodoHeader);
-        mAllTodoPanel.setMinWidth(Display.SCREEN_BOUNDS.getWidth() / 3.0);
         this.setLeft(mAllTodoPanel);
 
-        mBacklogTodoPanel = new VBox();
+        mBacklogTodoPanel = new TaskVBox("Backlog Todo");
         mBacklogTodoPanel.getStyleClass().add("panel_backlog-todo");
-        mBacklogTodoPanel.getChildren().add(mBacklogTodoHeader);
-        mBacklogTodoPanel.setMinWidth(Display.SCREEN_BOUNDS.getWidth() / 3.0);
         this.setCenter(mBacklogTodoPanel);
 
-        mDailyTodoPanel = new VBox();
+        mDailyTodoPanel = new TaskVBox("Daily Todo");
         mDailyTodoPanel.getStyleClass().add("panel_daily-todo");
-        mDailyTodoPanel.getChildren().add(mDailyTodoHeader);
-        mDailyTodoPanel.setMinWidth(Display.SCREEN_BOUNDS.getWidth() / 3.0);
         this.setRight(mDailyTodoPanel);
     }
 }
